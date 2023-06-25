f35667324c144cffc944ae9164926bee5db19c63
AlanHoltz <alanholtz1999@gmail.com>
2023-06-25 00:32:51 -0300

    Utilización de método replace de String en Ejercicio 1

--- 1/main.py
+++ 1/main.py
@@ -1,17 +1,5 @@
-import re
 from os import system
 import platform
-
-
-cadena = "Jacinta_Flores$Juan_Carlos_Feletti$Pedro_Lugones$Ana_María_Galíndez$\
-Juana_Bermudez$Rafael_Ernesto_Brahms$Beatriz_Valente$Ulma_Fabiana_Goya$Martina_Nicolesi$\
-Betania_Miraflores$Fermín_Olivetti$Ana_Luz_Narosky$Graciana_Arruabarrena$Joel_Pérez$\
-Valentina_Feller$Hector_Tadeo_Siemens$Natalia_Martinevsky$Ernesto_Nicolini$Pia_Paez$\
-Fermín_Obdulio_Camilo_Galíndez$Delfina_Beirut$Walter_Mantinoli$Celina_Celia_Samid$\
-Ulises_Malo$Juana_Varela$Melquíades_José_Li$Radamel_Servini$Filemón_Salsatti$Celeste_Faim$\
-Valerio_Martín_Rosseti$Jeremías_Farabutti$Verónica_Nefertiti$Ana_Delia_Pereyra$\
-Hermenilda_Carla_Rutini$Valerio_Tunuyán$Silvia_Solano$Beatriz_Bevacqua$Manuel_Martínez$\
-Berto_Carlos_Kigali$Juan_Manuel_Miraflores$Nicolás_Kligorsky$María_Laura_Berotti"
 
 
 def clear():
@@ -23,15 +11,23 @@ def clear():
 
 def main():
 
+    cadena = "Jacinta_Flores$Juan_Carlos_Feletti$Pedro_Lugones$Ana_María_Galíndez$\
+Juana_Bermudez$Rafael_Ernesto_Brahms$Beatriz_Valente$Ulma_Fabiana_Goya$Martina_Nicolesi$\
+Betania_Miraflores$Fermín_Olivetti$Ana_Luz_Narosky$Graciana_Arruabarrena$Joel_Pérez$\
+Valentina_Feller$Hector_Tadeo_Siemens$Natalia_Martinevsky$Ernesto_Nicolini$Pia_Paez$\
+Fermín_Obdulio_Camilo_Galíndez$Delfina_Beirut$Walter_Mantinoli$Celina_Celia_Samid$\
+Ulises_Malo$Juana_Varela$Melquíades_José_Li$Radamel_Servini$Filemón_Salsatti$Celeste_Faim$\
+Valerio_Martín_Rosseti$Jeremías_Farabutti$Verónica_Nefertiti$Ana_Delia_Pereyra$\
+Hermenilda_Carla_Rutini$Valerio_Tunuyán$Silvia_Solano$Beatriz_Bevacqua$Manuel_Martínez$\
+Berto_Carlos_Kigali$Juan_Manuel_Miraflores$Nicolás_Kligorsky$María_Laura_Berotti"
+
     clear()
 
-    cadena_cpy = str(cadena)
-    
-    cadena_cpy = re.sub("_"," ",cadena_cpy)
-    
-    cadena_cpy = re.sub("\$"," - ", cadena_cpy)
+    cadena = cadena.replace("_"," ")
 
-    alumnos = cadena_cpy.split(" - ")
+    cadena = cadena.replace("$"," - ")
+
+    alumnos = cadena.split(" - ")
 
     for alumno in alumnos:
         print(f"{alumno}\n")
